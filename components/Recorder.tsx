1566c1f5f52916495f14cd23d5dba3e1d6835f89
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-04-07 23:37:31 -0700

    initial commit

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,17 +1,19 @@
 'use client'
 import Image from "next/image"
 import activeAssistantIcon from "@/img/active.gif"
-import notActiveAssistantIcon from "@/img/noactive.gif"
+import notActiveAssistantIcon from "@/img/notactive.png"
 import { useEffect, useRef, useState } from "react";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
-export const mimeType = "audio/webm"
+export const mimeType = "audio/webm";
 function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
-    const MediaRecorder = useRef<MediaRecorder | null>(null);
-    const { pending } = useFormState();
+    
+    const mediaRecorder = useRef<MediaRecorder | null>(null);
+    const { pending } = useFormStatus();
     const [permission, setPermission] = useState(false);
-    const [stream, setStream] = useState(false);
-    const [recordingStatus, setRecordingStatus] = useState(false);
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
 
     useEffect(() => {
         getMicrophonePermission();
@@ -34,24 +36,34 @@ function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
     }
 
     const startRecording = async () => {
-        if (stream === null || pending) return;
+        if (stream === null || pending ) return;
 
         setRecordingStatus("recording");
 
         //create a new media recorder instance using the stream
         const media = new MediaRecorder(stream, { mimeType });
         mediaRecorder.current = media;
-        mediaRecorder.current = media;
+        mediaRecorder.current.start();
 
         let localAudioChunks: Blob[] = [];
-        MediaRecorder.current.ondataavailable = (event) => {
+        mediaRecorder.current.ondataavailable = (event) => {
             if (typeof event.data === "undefined") return;
             if (event.data.size === 0) return;
 
             localAudioChunks.push(event.data);
         };
 
-        setAudioChunks(localAudioChunks)
+        setAudioChunks(localAudioChunks);
+    }
+
+    const stopRecording = async () => {
+        if (mediaRecorder.current ===null || pending) return;
+
+        setRecordingStatus("inactive")
+        mediaRecorder.current.stop();
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: mimeType })
+        }
     }
 
     return (
@@ -68,7 +80,21 @@ function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
                     height={350}
                     priority
                     alt="Recording"
-                    className="grayscale"
+                    className="assistant cursor-pointer hover:scale-110 
+                duration-150 transition-all ease-in-out"
+                />
+            )}
+
+            {permission && recordingStatus === "inactive" && !pending && (
+                <Image src={notActiveAssistantIcon}
+
+                    width={350}
+                    height={350}
+                    priority={true}
+                    onClick={startRecording}
+                    alt="Not Recording"
+                    className="assistant cursor-pointer hover:scale-110 
+                duration-150 transition-all ease-in-out"
                 />
             )}
         </div>
